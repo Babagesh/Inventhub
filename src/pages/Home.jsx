8a7b73c8f60e9945fdbc1defad8b5ae5ceb896da
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2025-08-05 01:53:30 -0400

    setup project layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Search from '../components/Search'
+import {useState, useEffect} from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <Search />
+            // Grid
+            // Sort based on search filter
+        </div>
+    )
+}
+
+export default Home;
